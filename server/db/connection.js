b409df817390e107fb0f825e567a55495a5fd0e3
Utkarsh7368 <121254368+Utkarsh7368@users.noreply.github.com>
2024-05-08 23:38:58 +0530

    Update connection.js

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,2 +1,2 @@
 const mongoose =require('mongoose');
-mongoose.connect('mongodb+srv://pg491467:<password>@mernstack.e2lamat.mongodb.net/?retryWrites=true&w=majority&appName=MernStack');
+mongoose.connect('mongodb+srv://pg491467:test123@mernstack.e2lamat.mongodb.net/test?retryWrites=true&w=majority&appName=MernStack');
